027139e434ba989b7fa744c03d3508bc5d620cda
Cozmu <www.jorgereis@gmail.com>
2023-06-19 17:03:20 -0300

    test: testes da classe ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,13 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
-
-# Req 1
 def test_ingredient():
-    pass
+    instance_ingredient_x = Ingredient('farinha')
+
+    assert isinstance(instance_ingredient_x, Ingredient)
+    assert instance_ingredient_x.name == 'farinha'
+    assert instance_ingredient_x.__repr__() == "Ingredient('farinha')"
+    assert instance_ingredient_x.__eq__(Ingredient('farinha')) == True
+    assert instance_ingredient_x.__eq__(Ingredient('ovo')) == False
+    assert instance_ingredient_x.__hash__() != Ingredient('ovo').__hash__()
+    assert instance_ingredient_x.__hash__() == Ingredient('farinha').__hash__()
+    assert instance_ingredient_x.restrictions == {Restriction.GLUTEN}
