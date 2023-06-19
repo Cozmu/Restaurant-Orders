7b682ec5e74037eb2b4b89bec9e264f98051a2b2
Cozmu <www.jorgereis@gmail.com>
2023-06-19 19:11:01 -0300

    docs: anotação sobre importação do pytest

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,6 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
 from src.models.ingredient import Ingredient, Restriction
-import pytest
+import pytest # mudar interpretador > python CTRL + SHIFT + P > python: select interpreter
 
 def test_dish():
     with pytest.raises(TypeError):
