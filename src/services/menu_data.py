5c1558b8592b10d310d0fffe6b79e55fc6c9f73b
Cozmu <www.jorgereis@gmail.com>
2023-06-20 13:50:24 -0300

    feat: implementação da classe MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,32 @@
-# Req 3
+import csv
+
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
+def create_menu(path):
+    result = set()
+    dishes_dict = {}
+
+    with open(path, encoding = "utf-8") as file:
+        for line in csv.DictReader(file):
+            dish, price, ingredient, recipe_amount = line.values()
+            if dish in result:
+                new_ingredient = Ingredient(ingredient)
+                dishes_dict[dish].add_ingredient_dependency(
+                    new_ingredient, int(recipe_amount))
+            else:
+                new_dish = Dish(dish, float(price))
+                new_ingredient = Ingredient(ingredient)
+                new_dish.add_ingredient_dependency(
+                    new_ingredient, int(recipe_amount))
+                dishes_dict[dish] = new_dish
+                result.add(dish)
+
+    result = set(dishes_dict.values())
+    return result
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = create_menu(source_path)
